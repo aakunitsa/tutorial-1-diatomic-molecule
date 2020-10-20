e473f8609d2b2b8da8b002739134ff10fb4b3056
Alexander Kunitsa <28026247+aakunitsa@users.noreply.github.com>
2020-10-20 08:57:02 -0400

    added a function to generate molecular geometries along PES

--- diatomicmolecule.py
+++ diatomicmolecule.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 
 def create_diatomic_molecule_geometry(species1, species2, bond_length):
@@ -27,4 +28,22 @@ def create_diatomic_molecule(species1, species2, bond_length):
     geometry = create_diatomic_molecule_geometry(species1, species2, bond_length)
     geometry["schema"] = "molecular_geometry"
     with open("molecule.json", "w") as f:
+        f.write(json.dumps(geometry))
+
+
+def create_multiple_diatomic_molecules(species1, species2, bond_length, n_points, scale1, scale2):
+    init_bond_length = scale1 * bond_length 
+    final_bond_length = scale2 * bond_length 
+
+    geometry = {}
+    geometry["schema"] = "molecular_geometry_along_pes"
+    geometry["n_points"] = n_points
+    geometry["geometry_along_pes"] = {}
+    
+
+    for idx, r in enumerate(np.linspace(init_bond_length, final_bond_length, n_points)):
+        single_point_geometry = create_diatomic_molecule_geometry(species1, species2, r)
+        geometry["geometry_along_pes"][idx] = single_point_geometry
+        
+    with open("molecule_pes.json".format(str(idx)), "w") as f:
         f.write(json.dumps(geometry))
